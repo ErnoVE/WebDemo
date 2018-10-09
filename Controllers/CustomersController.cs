4e19cfb5902f85d1e3aca70a43785c4ce2e80da7
Jani Järvinen <jani.jarvinen@intertechno.org>
2018-10-09 14:42:28 +0300

    Lisätty entity-malli.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebDemoTurku.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        
+        
+
+
+    }
+}
